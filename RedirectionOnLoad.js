bf0e08305767a39e3fb71b3a49700d3b9257a34f
Dylan C <hchen004@citymail.cuny.edu>
2017-08-27 01:16:31 -0400

    Create RedirectionOnLoad.js

---
+++ RedirectionOnLoad.js
@@ -0,0 +1,8 @@
+'use strict';
+var redirect = (url)=>{
+    window.location.href = url;
+};
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  redirect('http://ccnytelevisionjournalism.com');
+});
